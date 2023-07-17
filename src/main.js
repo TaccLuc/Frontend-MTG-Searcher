693835c2b64ff2bae37cfb020ab59884561071ec
TaccLuc <128334060+TaccLuc@users.noreply.github.com>
2023-07-17 18:52:48 +0200

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,5 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import * as bootstrap from 'bootstrap';
+
+createApp(App).mount('#app');
---
+++ src/store.js
@@ -0,0 +1,5 @@
+import {store} from 'vue'
+
+export const store = reactive ({
+
+});
